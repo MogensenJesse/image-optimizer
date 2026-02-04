cb8e54fbc95635038bb3e8a2a05635da03139805
MogensenJesse <71662157+MogensenJesse@users.noreply.github.com>
2026-02-04 16:59:29 +0100

    Added UI versioning

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,9 +1,16 @@
+import { getVersion } from "@tauri-apps/api/app";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import { useEffect, useState } from "react";
 import closeIcon from "../assets/icons/close.svg";
 import minimizeIcon from "../assets/icons/minimize.svg";
 
 function TitleBar() {
   const appWindow = getCurrentWindow();
+  const [version, setVersion] = useState("");
+
+  useEffect(() => {
+    getVersion().then(setVersion).catch(console.error);
+  }, []);
 
   const handleMinimize = () => {
     appWindow.minimize();
@@ -16,7 +23,8 @@ function TitleBar() {
   return (
     <div className="title-bar" data-tauri-drag-region>
       <h1 className="title-bar-title" data-tauri-drag-region>
-        Image optimizer
+        Image optimizer{" "}
+        {version && <span className="title-bar-version">v{version}</span>}
       </h1>
       <div className="window-controls">
         <button
